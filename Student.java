97a9a42903c1989121bac3ea3ba6b348c30845cc
AJ <63066039+ajworks13@users.noreply.github.com>
2022-06-22 22:04:49 -0400

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,11 @@
+public class Student extends User{
+    void verify(){
+        System.out.println("Verifying through email");
+        set_verified(true);
+    }
+}public class Student extends User{
+    void verify(){
+        System.out.println("Verifying through email");
+        set_verified(true);
+    }
+}
